e9535395012959f6753d5502f928ba2199c8c4d6
thorne7 <darienhallam@gmail.com>
2023-01-17 17:28:37 +1030

    storing to local

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
 var today = dayjs();
-$("#currentDay").text(today.format('MMM D, YYYY'))
+$("#currentDay").text(today.format('dddd, MMMM D H:mm A'))
 
-$(function () {
+var hour = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+$(".saveBtn").click(saveButton);
+
+function saveButton() {
+ event.preventDefault();
+ var description = ('description').value;
+ var hourData = $(this).siblings(".description").val();
+ console.log("this worked")
+
+
+
+  localStorage.setItem(hourData, description);
+ }
+
+
+
+    // <script>
+    //         function saveData(){
+    //     localStorage.saveServer
+    //     }
+    //     </script>
+  
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -24,4 +45,33 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+  // };
+
+// $(".saveBtn").click(function () {
+
+// for (var i = 0; i < hour.length; i++) {
+//   // var dataHour = localStorage.getItem(x[i]);
+
+// if ('hour')dayjs().isBefore(dayjs("H")) 
+//   $(".time-block").addClass("past"); 
+
+// if ('hour')dayjs().isSame(dayjs("H"))
+// $(".time-block").addClass("present");
+
+// if ('hour')dayjs().isAfter(dayjs("H"))
+// $(".time-block").addClass("future");
+
+// }})
+
+
+// $(".saveBtn").click(function () {
+//   if ('hour')dayjs().isSame(dayjs("H")); {
+//     $(".time-block").addClass("present");
+//   }
+//     })
+    
+//     $(".saveBtn").click(function () {
+//   if ('hour')dayjs().isAfter(dayjs("H")); {
+//     $(".time-block").addClass("future");
+//   }
+//     })
